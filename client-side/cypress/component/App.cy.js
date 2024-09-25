2453052d17f8ada84bb1911dd59154f879d7a74e
Damon Martin <damon.martin@students.plymouth.ac.uk>
2024-09-25 13:34:12 +0100

    Tests developed further and adding git ignore

--- client-side/cypress/component/App.cy.js
+++ client-side/cypress/component/App.cy.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import App from '../../src/pages/App'
+import { assert, expect, should } from 'chai';
 
+// App: Page Component
+// LoginForm: Form Component 
 describe('<App />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
+  it('test-page-component-renders', () => {
     cy.mount(<App />)
   })
+
+  it('test-login-form-is-mounted-in-page-component', () => {
+    assert.fail('Incomplete Test');
+  });
+
+  it('test-login-form-is-in-center-of-App', () => {
+    assert.fail('Incomplete Test');
+  });
 })
